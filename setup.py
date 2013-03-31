4e7dcf935ff2eaaa57f28fbd259268452ae88b55
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2013-03-31 11:54:59 +0300

    Updated trove classifiers, pyflakes.

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ name = 'hexagonit.recipe.download'
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(name=name,
-      version=version,
-      description="zc.buildout recipe for downloading and extracting packages",
-      long_description=(
+setup(
+    name=name,
+    version=version,
+    description="zc.buildout recipe for downloading and extracting packages",
+    long_description=(
         read('README.rst')
         + '\n' +
         'Detailed Documentation\n'
@@ -28,30 +29,36 @@ setup(name=name,
         + '\n' +
         'Download\n'
         '***********************\n'
-        ),
-      classifiers=[
-       'Framework :: Buildout',
-       'Intended Audience :: Developers',
-       'License :: OSI Approved :: Zope Public License',
-       'Topic :: Software Development :: Build Tools',
-       'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-      keywords='development buildout recipe',
-      author='Kai Lautaportti',
-      author_email='kai.lautaportti@hexagonit.fi',
-      url='http://github.com/hexagonit/%s' % name,
-      license='ZPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['hexagonit', 'hexagonit.recipe'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+    ),
+    classifiers=[
+        'Framework :: Buildout :: Recipe',
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='development buildout recipe',
+    author='Kai Lautaportti',
+    author_email='kai.lautaportti@hexagonit.fi',
+    url='http://github.com/hexagonit/%s' % name,
+    license='ZPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['hexagonit', 'hexagonit.recipe'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'zc.buildout >= 1.4.0',
-        'setuptools'],
-      extras_require={
+        'setuptools',
+    ],
+    extras_require={
         'test': ['zope.testing'],
-      },
-      tests_require=['zope.testing'],
-      test_suite='hexagonit.recipe.download.tests.test_suite',
-      entry_points={'zc.buildout': ['default = hexagonit.recipe.download:Recipe']},
-      )
+    },
+    tests_require=['zope.testing'],
+    test_suite='hexagonit.recipe.download.tests.test_suite',
+    entry_points={'zc.buildout': ['default = hexagonit.recipe.download:Recipe']},
+)
